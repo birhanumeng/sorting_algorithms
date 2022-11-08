ecc55a08708b7fbff214da4fab2ee4ecc9ebc88f
birhanumeng <eyerusalemmengiste@gmail.com>
2022-11-07 23:30:20 -0800

    header file

--- sort.h
+++ sort.h
@@ -1,6 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
