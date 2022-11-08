f658fc44388819baee7f753f97cb5da69e1ab898
birhanumeng <eyerusalemmengiste@gmail.com>
2022-11-07 23:03:27 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sort array of integers in assending order
+ *               using bubble sort
+ * array: pointer to integer array
+ * size: array size
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, tmp;
+	bool swapped = true;
+
+	while (swapped)
+	{
+		swapped = false;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				swapped = true;
+			}
+		}
+	}
+
+	return;
+}
